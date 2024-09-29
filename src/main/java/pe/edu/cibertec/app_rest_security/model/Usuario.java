346f22c218c5198af8cab5e370b64abd909d482b
AsumaToki04 <megamanzxu@hotmail.com>
2024-09-29 14:08:30 -0500

    Clases Rol 1 y Usuario 1

--- src/main/java/pe/edu/cibertec/app_rest_security/model/Usuario.java
+++ src/main/java/pe/edu/cibertec/app_rest_security/model/Usuario.java
@@ -1,4 +1,21 @@
 package pe.edu.cibertec.app_rest_security.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@Table(name = "usuario")
 public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idusuario;
+    private String nomusuario;
+    private String email;
+    private String nombres;
+    private String apellidos;
+    private String password;
+    private Boolean activo;
 }
