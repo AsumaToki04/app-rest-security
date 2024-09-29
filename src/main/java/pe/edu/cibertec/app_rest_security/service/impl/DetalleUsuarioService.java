ffe05eebb1288ed0692b04a222dee3357772dc92
AsumaToki04 <megamanzxu@hotmail.com>
2024-09-29 15:00:25 -0500

    Clase Service DetalleUsuario 2 y Clase Filtro JWTAuth 1

--- src/main/java/pe/edu/cibertec/app_rest_security/service/impl/DetalleUsuarioService.java
+++ src/main/java/pe/edu/cibertec/app_rest_security/service/impl/DetalleUsuarioService.java
@@ -3,6 +3,7 @@ package pe.edu.cibertec.app_rest_security.service.impl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -33,5 +34,18 @@ public class DetalleUsuarioService implements UserDetailsService {
                     "ROLE_" + rol.getNomrol()));
         }
         return rolesAuth;
+    }
+
+    private UserDetails crearUserDetail(
+            Usuario usuario, List<GrantedAuthority> authorityList
+    ) {
+        return new User(
+                usuario.getNomusuario(),
+                usuario.getPassword(),
+                usuario.getActivo(),
+                true,
+                true,
+                true,
+                authorityList);
     }
 }
