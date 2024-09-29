d551567254bbeb1dc92bda9867eecec1774673da
AsumaToki04 <megamanzxu@hotmail.com>
2024-09-29 14:46:22 -0500

    Interface Service Usuario 1 y Clases Service Usuario 1 y DetalleUsuario 1

--- src/main/java/pe/edu/cibertec/app_rest_security/service/impl/DetalleUsuarioService.java
+++ src/main/java/pe/edu/cibertec/app_rest_security/service/impl/DetalleUsuarioService.java
@@ -1,4 +1,37 @@
 package pe.edu.cibertec.app_rest_security.service.impl;
 
-public class DetalleUsuarioService {
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+import pe.edu.cibertec.app_rest_security.model.Rol;
+import pe.edu.cibertec.app_rest_security.model.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+
+@Service
+@RequiredArgsConstructor
+public class DetalleUsuarioService implements UserDetailsService {
+    private final UsuarioService usuarioService;
+    @Override
+    public UserDetails loadUserByUsername(String username)
+            throws UsernameNotFoundException {
+        Usuario usuario = usuarioService.obtenerUsuarioXnombre(username);
+
+        return null;
+    }
+
+    public List<GrantedAuthority> obtenerRolesUsuario(Set<Rol> rolesList) {
+        List<GrantedAuthority> rolesAuth = new ArrayList<>();
+        for(Rol rol: rolesList) {
+            rolesAuth.add(new SimpleGrantedAuthority(
+                    "ROLE_" + rol.getNomrol()));
+        }
+        return rolesAuth;
+    }
 }
